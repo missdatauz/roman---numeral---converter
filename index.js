d2ee0addcb3a055e510ad3df1460b01603fc2d1f
missdatauz <mahliyoshermetova12@gmail.com>
2026-01-07 11:11:02 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,31 @@
-const inputNumber = document.getElementById("number");
+const number = document.getElementById("number");
 const convertBtn = document.getElementById("convert-btn");
 const output = document.getElementById("output");
 
-convertBtn.addEvenetListener("click", () => {
- const inputValue = inputNumber.ariaValueMax.trim();
+const convertToRoman = () => {
+  if (number.value == "") {
+     output.innerHTML = "Please enter a valid number";
+     return;
+  } else if (number.value < 1) {
+     output.innerHTML = "Please enter a number greater than or equal to 1";
+     return;
+  }
+  if (number.value > 3999) {
+     output.innerHTML = "Please enter a number less than or equal to 3999";
+     return;
+  }
+   const romanSymbols = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"];
 
- if (inputValue === "") {
-    output.textContent = "Please enter a valid number";
-    output.classList.remove("hidden");
-    return;
- }
-const num = parseInt(inputValue, 10)
+   const romanValues = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
 
-})
+   let romanNumeral = "";
+
+   for(let i = 0; i < romanValues.length; i++) {
+  while (number.value >= romanValues[i]) {
+     romanNumeral += romanSymbols[i];
+     number.value -= romanValues[i]
+  }   
+   }
+output.innerHTML = romanNumeral;
+}
+convertBtn.addEventListener("click", convertToRoman)
